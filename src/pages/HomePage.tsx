8f107c52c0fbecfac344e730cdac5c2641bae106
unknown <ppsplash13@gmail.com>
2026-01-23 10:57:28 +0100

    search done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Artwork } from "../types";
-import { searchArt } from "../data";
+import { basicArt, searchArt } from "../data";
 import ArtworkCard from "../components/UI/ArtworkCard";
 
 interface ArtResponse {
@@ -16,10 +16,29 @@ function HomePage() {
     data: [],
     pagination: { total: 0, current_page: 1 },
   });
+
+  const searchInput = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const input = new FormData(e.currentTarget);
+    const keyword = input.get("keyword") as string;
+    console.log("Searching for:", keyword);
+    try {
+      const results = await searchArt(keyword);
+      setArt((prev) => ({
+        ...prev,
+        data: results,
+      }));
+      // Clear the form ONLY if the search was successful
+      e.currentTarget.reset();
+    } catch (err) {
+      console.error("Search failed:", err);
+    }
+  };
+
   useEffect(() => {
     const FetchArtwork = async () => {
       try {
-        const newData = await searchArt();
+        const newData = await basicArt();
         console.log(newData);
         setArt((prev) => ({
           ...prev,
@@ -35,18 +54,21 @@ function HomePage() {
   return (
     <div className="p-8">
       <header className="text-center flex flex-col items-center gap-6 mb-12">
-        <h1 className="text-6xl font-bold">Search your Artworks</h1>
+        <h1 className="text-6xl font-bold text-center">Find Artworks here</h1>
 
-        {/* Moved search bar here so it doesn't sit inside the art grid */}
-        <label className="input input-bordered flex items-center gap-2 w-full max-w-md">
-          <input type="search" className="grow" placeholder="Search Artworks..." />
-          <svg className="h-4 w-4 opacity-70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.3-4.3" />
-            </g>
-          </svg>
-        </label>
+        <form onSubmit={searchInput} className="w-full max-w-md">
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="search" name="keyword" className="grow" placeholder="Search Artworks..." />
+            <button type="submit">
+              <svg className="h-4 w-4 opacity-70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <circle cx="11" cy="11" r="8" />
+                  <path d="m21 21-4.3-4.3" />
+                </g>
+              </svg>
+            </button>
+          </label>
+        </form>
       </header>
 
       {/* Grid for Artworks */}
@@ -54,14 +76,6 @@ function HomePage() {
         {art.data.map((item) => (
           <ArtworkCard key={item.id} item={item} />
         ))}
-        {/* <div key={item.id} className="card bg-base-100 shadow-xl border p-4 flex flex-col gap-4">
-            <img src={imageUrl(item.image_id)} alt={item.title} className="w-full h-64 object-cover rounded-lg bg-gray-100" />
-            <div className="flex flex-col gap-1">
-              <h3 className="text-lg font-bold leading-tight line-clamp-2">{item.title}</h3>
-              <p className="text-sm text-gray-500 italic">{item.artist_title || "Unknown Artist"}</p>
-            </div>
-          </div>
-        ))} */}
       </section>
     </div>
   );
